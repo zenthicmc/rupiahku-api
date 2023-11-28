754c048272663205f4affc1bc689fa6a4bb5a91c
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-11-28 16:52:35 +0700

    Update sendSms.js

--- src/helpers/sendSms.js
+++ src/helpers/sendSms.js
@@ -1,9 +1,9 @@
-const sendSms = (number, code) => {
+const sendSms = async (number, code) => {
 	const accountSid = "AC423f9bb5a3ebe4f05e7865270790933d";
   const authToken = "ee9f8aa648ebbe80a075de416ac81746";
   const client = require("twilio")(accountSid, authToken);
 
-  client.messages
+  await client.messages
     .create({
       body: "Silahkan verifikasi akun anda dengan memasukkan kode ini "+ code,
       from: "+14708655183",
