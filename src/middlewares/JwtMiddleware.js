b24ee1bd9e41defb20b3d94ed23972d0f6a10a4a
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-12-26 20:13:28 +0700

    Update JwtMiddleware.js

--- src/middlewares/JwtMiddleware.js
+++ src/middlewares/JwtMiddleware.js
@@ -10,16 +10,16 @@ async function JwtMiddleware (req, res, next) {
 		const token = req.headers.authorization.split(' ')[1];
 		const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-		// verification step 1
+		// verification step 1 = verify token by key
 		const decoded = jwt.verify(token, process.env.JWT_KEY);
 		
-		// verification step 2
+		// verification step 2 = verify token by user id
 		const user = await User.findOne({
 			_id: decoded.sub,
 		});
 		if(!user) return response401(res);
 
-		// verification step 3
+		// verification step 3 = verify token by same token
 		if(user.token !== token) return response401(res);
 
 		next();
